b0bc92f856a1e3f42b6e3cb5cd7038545f9799c2
Vito_RF_ARC <vitoraditya.f@gmail.com>
2024-07-22 21:50:27 +0700

    Delete vite-portfolio directory

--- vite-portfolio/src/components/Cv.jsx
+++
@@ -1,8 +0,0 @@
-export default function Cv() {
-    return (
-        <div className="w-4/5 mx-auto bg-slate-200">
-            <h1>Experiances</h1>
-            <p>CLick below to access my CV</p>
-        </div>
-    )
-}
--- vite-portfolio/src/components/Separator.jsx
+++
@@ -1,7 +0,0 @@
-export default function Separator() {
-  return (
-    <div className="separator">
-      <span className="text-slate-50">s</span>
-    </div>
-  );
-}
