4a565209100da26e8d7b731ab02d602e4e9b9085
vitoradityafauzan <vitoraditya.f@gmail.com>
2024-07-23 17:38:04 +0700

    V-0.3, Projects and Skills done. Contactlist mobile broken after a href

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <div className="w-4/5 mx-auto bg-slate-100" id="contacts">
+        <div className="w-4/5 mx-auto " id="contacts">
             <h1 id="section-title">Contact</h1>
             <h1 className="font-mono font-light text-lg">Want to get in touch? Contact me on any of the platform</h1>
         </div>
--- src/components/common/ContactLinks.jsx
+++ src/components/common/ContactLinks.jsx
@@ -8,12 +8,24 @@ import linkedin from "../../assets/icons/linkedin.webp";
 export default function ContactLinks() {
   return (
     <div className="w-4/5 mx-auto mt-4 border-0 flex flex-cols justify-center gap-4 md:gap-12 xl:gap-20">
-      <img src={tele} alt="tele" className="w-[10%] md:w-8 lg:w-12 border-0" />
-      <img src={github} alt="github" className="w-[10%] md:w-8 lg:w-12 border-0" />
-      <img src={gitlab} alt="gitlab" className="w-[10%] md:w-8 lg:w-12 border-0" />
-      <img src={whatsapp} alt="whatsapp" className="w-[10%] md:w-8 lg:w-12 border-0" />
-      <img src={twitter} alt="twitter" className="w-[10%] md:w-8 lg:w-12 border-0" />
-      <img src={linkedin} alt="linkedin" className="w-[10%] md:w-8 lg:w-12" />
+      <a href="https://t.me/Hermaeus_M0ra" target="_blank" rel="noopener noreferrer" className="w-[10%] md:w-8 lg:w-12 border-0">
+        <img src={tele} alt="tele" className="w-[10%] md:w-8 lg:w-12 border-0" />
+      </a>
+      <a href="https://github.com/vitoradityafauzan" target="_blank" rel="noopener noreferrer"  className="w-[10%] md:w-8 lg:w-12 border-0">
+        <img src={github} alt="github" className="w-[10%] md:w-8 lg:w-12 border-0" />
+      </a>
+      <a href="https://gitlab.com/vitoradityafauzan" target="_blank" rel="noopener noreferrer"  className="w-[10%] md:w-8 lg:w-12 border-0">
+        <img src={gitlab} alt="gitlab" className="w-[10%] md:w-8 lg:w-12 border-0" />
+      </a>
+      <a href="https://wa.me/qr/YTAIGLZ2GIZYC1" target="_blank" rel="noopener noreferrer"  className="w-[10%] md:w-8 lg:w-12 border-0">
+        <img src={whatsapp} alt="whatsapp" className="w-[10%] md:w-8 lg:w-12 border-0" />
+      </a>
+      <a href="https://twitter.com/Vito_Radtya" target="_blank" rel="noopener noreferrer"  className="w-[10%] md:w-8 lg:w-12 border-0">
+        <img src={twitter} alt="twitter" className="w-[10%] md:w-8 lg:w-12 border-0" />
+      </a>
+      <a href="https://www.linkedin.com/in/vito-raditya-fauzan-b43833219/" target="_blank" rel="noopener noreferrer"  className="w-[10%] md:w-8 lg:w-12 border-0">
+        <img src={linkedin} alt="linkedin" className="w-[10%] md:w-8 lg:w-12" />
+      </a>
     </div>
   );
 }
