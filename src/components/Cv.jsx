51d436b0cc824939222f4b7054338aff4f087396
vitoradityafauzan <vitoraditya.f@gmail.com>
2024-07-23 18:09:19 +0700

    V1.0, Skills redone, experiance done, contactlist redone, footer redone

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -2,12 +2,10 @@ import {Button} from "flowbite-react"
 
 export default function Cv() {
   return (
-    <div className="w-4/5 mx-auto bg-slate-200" id="experience">
-      <h1>Experiances</h1>
-      <p>CLick below to access my CV</p>
-      <a href="#header">
-        <Button size="xl" gradientDuoTone="purpleToBlue"><h1 className="text-2xl my-1">MY CV</h1></Button>
-      </a>
+    <div className="w-4/5 mx-auto " id="experience">
+      <h1 id="section-title">Experiances</h1>
+      <p className="text-center text-xl mt-7 mb-9">Click below to access my CV</p>
+        <Button size="lg" gradientDuoTone="purpleToBlue" href="https://drive.google.com/file/d/1YliXYK_RpZ-DwpMf3PVjSuTR1kDjdKh4/view?usp=sharing" className="w-36 mx-auto"><h1 className="text-2xl my-2">MY CV</h1></Button>
     </div>
   );
 }
--- src/components/Footerr.jsx
+++ src/components/Footerr.jsx
@@ -4,12 +4,12 @@ export default function Footerr() {
   return (
     <Footer container className="mt-11 sm:justify-between md:justify-center md:gap-24 lg:gap-96">
       <Footer.Copyright href="#" by="Vito RF" year={2024} />
-      <Footer.LinkGroup className="text-xl font-semibold text-blue-600">
-        <Footer.Link href="#header">About</Footer.Link>
+      <Footer.LinkGroup className="text-xl font-semibold text-blue-600 justify-start w-auto">
+        {/* <Footer.Link href="#header">About</Footer.Link> */}
         <Footer.Link href="#projects">Projects</Footer.Link>
         <Footer.Link href="#skills">Skills</Footer.Link>
         <Footer.Link href="#experience">Experience</Footer.Link>
-        <Footer.Link href="#contacts">Contact</Footer.Link>
+        {/* <Footer.Link href="#contacts">Contact</Footer.Link> */}
       </Footer.LinkGroup>
     </Footer>
   );
