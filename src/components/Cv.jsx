a96a9aeacda88c89fb73e5e63b6eb5a89f45d062
Vito_RF_ARC <vitoraditya.f@gmail.com>
2025-03-05 16:40:46 +0700

    refactor: change cv link

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -5,7 +5,7 @@ export default function Cv() {
     <div className="w-4/5 mx-auto " id="experience">
       {/* <h1 id="section-title">Experiances</h1> */}
       <p className="text-center text-xl mt-7 mb-9">Click below to access my CV</p>
-        <Button size="lg" gradientDuoTone="purpleToBlue" href="https://drive.google.com/file/d/1YliXYK_RpZ-DwpMf3PVjSuTR1kDjdKh4/view?usp=sharing" className="w-36 mx-auto"><h1 className="text-2xl my-2">MY CV</h1></Button>
+        <Button size="lg" gradientDuoTone="purpleToBlue" href="https://drive.google.com/file/d/1hnsZFni50Ds_SW0FJpXH3EAY3AfhGhe1/view?usp=sharing" className="w-36 mx-auto"><h1 className="text-2xl my-2">MY CV</h1></Button>
     </div>
   );
 }
